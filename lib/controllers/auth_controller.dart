ef46e7da88798fb93853ecb2bc39bd7e4f7e8b90
Jungwoon <jwoon2004@gmail.com>
2023-07-17 02:52:57 +0900

    bottomNavigationBar complete

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -7,12 +7,31 @@ import 'package:image_picker/image_picker.dart';
 import 'package:tiktok/constants.dart';
 import 'package:tiktok/models/user.dart' as model;
 
+import '../views/screens/auth/login_screen.dart';
+import '../views/screens/home_screen.dart';
+
 class AuthController extends GetxController {
   static AuthController instance = Get.find();
-
+  late Rx<User?> _user;
   late Rx<File?> _pickedImage;
 
   File? get ProfilePhoto => _pickedImage.value;
+
+  @override
+  void onReady() {
+    super.onReady();
+    _user = Rx<User?>(firebaseAuth.currentUser);
+    _user.bindStream(firebaseAuth.authStateChanges());
+    ever(_user, _setInitialScreen);
+  }
+
+  _setInitialScreen(User? user) {
+    if (user == null) {
+      Get.offAll(() => LoginScreen());
+    } else {
+      Get.offAll(() => const HomeScreen());
+    }
+  }
 
   void pickImage() async {
     final pickedImage =
