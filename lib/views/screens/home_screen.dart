0cc04da4fc5643bba7973f4de5e21585322b85da
Jungwoon <jwoon2004@gmail.com>
2023-07-17 17:08:19 +0900

    uploading video done

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -17,46 +17,45 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-          onTap: (idx) {
-            setState(() {
-              pageIdx = idx;
-            });
-          },
-          backgroundColor: backgroundColor,
-          selectedItemColor: Colors.red,
-          unselectedItemColor: Colors.white,
-          currentIndex: pageIdx,
-          type: BottomNavigationBarType.fixed,
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.home,
-                  size: 30,
-                ),
-                label: 'Home'),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.search,
-                  size: 30,
-                ),
-                label: 'Search'),
-            BottomNavigationBarItem(icon: CustomIcon(), label: ''),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.message,
-                  size: 30,
-                ),
-                label: 'Messages'),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.person,
-                  size: 30,
-                ),
-                label: 'Profile')
-          ]),
-      body: const Center(
-        child: Text('Home Screen'),
+        onTap: (idx) {
+          setState(() {
+            pageIdx = idx;
+          });
+        },
+        backgroundColor: backgroundColor,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.white,
+        currentIndex: pageIdx,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                size: 30,
+              ),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                size: 30,
+              ),
+              label: 'Search'),
+          BottomNavigationBarItem(icon: CustomIcon(), label: ''),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.message,
+                size: 30,
+              ),
+              label: 'Messages'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                size: 30,
+              ),
+              label: 'Profile')
+        ],
       ),
+      body: pages[pageIdx],
     );
   }
 }
